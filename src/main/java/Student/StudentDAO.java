635bd584b000a3db022a1bab6eb9f499f57eece5
Ashutoshsingh <ashutoshsingh.pardeshi@gurucul.com>
2025-06-20 18:59:55 +0530

    feat: adding DAO files

---
+++ src/main/java/Student/StudentDAO.java
@@ -0,0 +1,55 @@
+package Student;
+
+import java.sql.*;
+import java.util.*;
+
+public class StudentDAO {
+    private Connection conn;
+
+    public StudentDAO(Connection conn) {
+        // Establish connection to the DB
+        this.conn = conn;
+    }
+
+    public void addStudent(Student student) {
+        // prepared statement
+        String querystring = "INSERT INTO students (MIS, firstName, lastName) VALUES (?,?,?)";
+
+        try (PreparedStatement stmt = conn.prepareStatement(querystring)) {
+            // Extracting the data
+            stmt.setInt(1, student.getMIS());
+            stmt.setString(2, student.getFirstName());
+            stmt.setString(3, student.getLastName());
+
+            // Executing the query
+            stmt.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public List<Student> getAllStudents() {
+        List<Student> students = new ArrayList<>();
+
+        // prepared statement to fetch all students from the DB
+        String query = "SELECT * FROM students";
+
+        try (
+                PreparedStatement stmt = conn.prepareStatement(query);
+                ResultSet rs = stmt.executeQuery()) {
+            while (rs.next()) {
+                // Extrating the values from the DB
+                Student student = new Student(
+                        rs.getInt("MIS"),
+                        rs.getString("firstName"),
+                        rs.getString("lastName"));
+
+                // Add the student info to the list
+                students.add(student);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return students;
+    }
+}
